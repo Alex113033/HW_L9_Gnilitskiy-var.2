47e74792f172ba0d41e6d57d4a51572924ff9d32
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-05-14 13:05:19 +0300

    Initial Commit

---
+++ HW_L9_Gnilitskiy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_L9_Gnilitskiy
+//
+//  Created by Александр on 14.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
